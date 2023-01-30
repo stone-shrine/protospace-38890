63192c76c113a13cda3371e6f19123c5f1060354
stone-shrine <iso.5565.shnk@gmail.com>
2023-01-30 11:47:00 +0900

    空コメント投稿時のエラー修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,7 @@ class CommentsController < ApplicationController
     if @comment.save
       redirect_to prototype_path(@comment.prototype)
     else
-      render "prototypes/show"
+      redirect_to "/prototypes/#{@comment.prototype_id}"
     end
   end
 
